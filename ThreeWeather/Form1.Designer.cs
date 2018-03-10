68c9dfa51f5f4bcbd34b62baee1e26950c759eb5
StJester <stellarjester@gmail.com>
2018-03-10 02:26:20 +0200

    first commit

---
+++ ThreeWeather/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ThreeWeather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.City = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Temperature = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.InformatorLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.City,
+            this.Temperature});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(3, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Scrollable = false;
+            this.listView1.Size = new System.Drawing.Size(479, 141);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // City
+            // 
+            this.City.Text = "City";
+            this.City.Width = 239;
+            // 
+            // Temperature
+            // 
+            this.Temperature.Text = "Temperature";
+            this.Temperature.Width = 236;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(485, 160);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TimerLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.InformatorLabel);
+            this.groupBox2.Controls.Add(this.refreshButton);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 160);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(485, 184);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(174, 22);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(115, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(174, 81);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(115, 23);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // InformatorLabel
+            // 
+            this.InformatorLabel.AutoSize = true;
+            this.InformatorLabel.Location = new System.Drawing.Point(36, 57);
+            this.InformatorLabel.Name = "InformatorLabel";
+            this.InformatorLabel.Size = new System.Drawing.Size(54, 13);
+            this.InformatorLabel.TabIndex = 2;
+            this.InformatorLabel.Text = "Informator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(89, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last refreshed";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimerLabel.Location = new System.Drawing.Point(268, 140);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(80, 24);
+            this.TimerLabel.TabIndex = 2;
+            this.TimerLabel.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 344);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader City;
+        private System.Windows.Forms.ColumnHeader Temperature;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label InformatorLabel;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ThreeWeather/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreeWeather
+{
+    public struct WeatherCity
+    {
+        public string city;
+        public string tempr;
+
+        public WeatherCity(string _c, string _t)
+        {
+            city = _c;
+            tempr = _t;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        int S = 0, M = 0, H = 0;
+
+
+        string addrBarcelona = "https://www.gismeteo.ua/weather-barcelona-1948/";
+        string addrRio = "https://www.gismeteo.ua/weather-rio-de-janeiro-10452/";
+        string addrKiev = "https://www.gismeteo.ua/weather-kyiv-4944/";
+
+
+        HttpWebRequest query;
+        HttpWebResponse answer;
+        List<WeatherCity> _items = new List<WeatherCity>();
+
+
+        string codeToParce = "";
+        public Form1()
+        {
+            InitializeComponent();
+            //get weather info
+            ProcessWebSite(addrBarcelona, @"..\..\Barcelona.txt");
+            WriteInFile(@"..\..\Barcelona.txt", codeToParce);
+            Parcer(codeToParce);
+            
+            ProcessWebSite(addrRio, @"..\..\Rio.txt");
+            WriteInFile(@"..\..\Rio.txt", codeToParce);
+            Parcer(codeToParce);
+
+            ProcessWebSite(addrKiev, @"..\..\Kiev.txt");
+            WriteInFile(@"..\..\Kiev.txt", codeToParce);
+            Parcer(codeToParce);
+
+        }
+
+        private void Parcer(string codeToParce)
+        {
+            string temperatureFull = ParceTemperature(codeToParce);
+            
+            string cityName = ParceCity(codeToParce);
+
+            Display(new WeatherCity(cityName, temperatureFull));
+        }
+
+        private string ParceCity(string codeToParce)
+        {
+            string anchor = "section higher";
+            int length = anchor.Length;
+           
+            int pos = codeToParce.IndexOf(anchor) + length;
+
+            string sub = codeToParce.Substring(pos);
+
+            string name = sub.Split('>')[2];
+            
+            name = name.Split('<')[0];
+            
+            return name;
+        }
+
+        private string ParceTemperature(string codeToParce)
+        {
+            string anchor = "<dd class=\'value m_temp c\'>";
+            int lengh = anchor.Length;
+            
+            int pos = codeToParce.IndexOf(anchor) + lengh;
+
+            string sub = codeToParce.Substring(pos);
+            string sign = sub.Split(';')[0];
+
+            string target = sub.Split(';')[1];
+            string tempr = sub.Split('<')[0];
+
+            if (sign == "&minus")
+            {
+                tempr = $"-" + target.Split('<')[0];
+            }
+            return tempr;
+        }
+
+        private void Display(WeatherCity _item)
+        {
+            ListViewItem li = listView1.Items.Add(_item.city);
+            li.SubItems.Add(_item.tempr);
+        }
+
+
+        string ProcessWebSite(string addr, string path)
+        {
+            GetAnswer(addr);
+
+            codeToParce = SaveAnswer();
+
+            return codeToParce;
+        }
+
+        void GetAnswer(string _addr)
+        {
+            query = (HttpWebRequest)
+                HttpWebRequest.Create(_addr);
+            answer = (HttpWebResponse)
+                query.GetResponse();
+        }
+
+        string SaveAnswer()
+        {
+            Stream content = answer.GetResponseStream();
+            StreamReader sr = new StreamReader(content);
+            string code = sr.ReadToEnd();
+            sr.Close();
+            return code;
+        }
+
+        void WriteInFile(string _path, string _code)
+        {
+            FileStream fs = new FileStream(_path, FileMode.Create,
+                FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(_code);
+            sw.Close();
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+
+            if (listView1.Items != null)
+            {
+                listView1.Items.Clear();
+            }
+
+            ProcessWebSite(addrBarcelona, @"..\..\Barcelona.txt");
+            Parcer(codeToParce);
+
+            ProcessWebSite(addrRio, @"..\..\Rio.txt");
+            Parcer(codeToParce);
+
+            ProcessWebSite(addrKiev, @"..\..\Kiev.txt");
+            Parcer(codeToParce);
+
+            timer1.Start();
+            S = 0;
+            M = 0;
+            H = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            S++;
+            if(S >=59)
+            {
+                M++;
+                S = 0;
+            }
+            if(M>=59)
+            {
+                H++;
+                M = 0;
+            }
+
+            TimerLabel.Text = H.ToString() + ":" + M.ToString() + ":" + S.ToString();
+        }
+    }
+}
